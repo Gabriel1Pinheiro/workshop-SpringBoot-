18d3c15c65356eb517ca6dc19e4012e1d6f3710d
Gabriel <gabriel.pinheiro1240@gmail.com>
2023-12-25 22:45:39 -0300

    Exception handling - findById

---
+++ src/main/java/com/Projeto/SpringBoot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.Projeto.SpringBoot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. id " + id);
+    }
+
+}
